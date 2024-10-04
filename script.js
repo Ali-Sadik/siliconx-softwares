4d205beb721c8cb146227bb40c016b50264f32a0
Ali Ahmed Sadik <141386263+Ali-Sadik@users.noreply.github.com>
2024-10-05 05:34:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $("#myCarousel").carousel({
   interval: false
 });
+
+ // Function to preload the background image
+ function preloadBackgroundImage(url) {
+  return new Promise((resolve) => {
+      const img = new Image();
+      img.src = url;
+      img.onload = () => resolve();
+  });
+}
+
+// Preload the image and then show the content
+preloadBackgroundImage("black.jpg").then(() => {
+  document.body.style.display = 'block'; // Show body content
+  document.querySelector('.loading').style.display = 'none'; // Hide loading indicator
+  document.querySelector('.content').style.display = 'block'; // Show content
+});
